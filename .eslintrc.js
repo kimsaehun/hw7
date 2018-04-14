e23d5145a9da44d3d5c5e57d3b298609ebfffa13
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-04-14 16:43:46 -0700

    Complete hw 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "env": {
-        "browser": true
+        "browser": true,
+	"es6": true
+    },
+    "parserOptions": {
+	"ecmaVersion": 6,
+	"sourceType": "module"
     },
     "extends": "eslint:recommended",
     "rules": {
@@ -19,7 +24,8 @@ module.exports = {
         "semi": [
             "error",
             "always"
-        ]
+        ],
+	"no-console": "off"
     }
 };
 
